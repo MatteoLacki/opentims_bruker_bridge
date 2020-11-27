dde0331aaebf15b837b589bfff4d5796c7f5bce7
Michał Startek <michal.startek@mimuw.edu.pl>
2020-11-27 17:31:21 +0100

    Fix for Windoze stupid error handling by popup messages

--- opentims_bruker_bridge/__init__.py
+++ opentims_bruker_bridge/__init__.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 
 _abspath = os.path.abspath(__file__)
@@ -6,12 +7,21 @@ _mod_dir = os.path.dirname(_abspath)
 
 _opj = os.path.join
 
-_paths = [
-    _opj(_mod_dir, "libtimsdata.so"),
-    _opj(_mod_dir, "win32", "timsdata.dll"),
-    _opj(_mod_dir, "win64", "timsdata.dll")
-    ]
+
+
+_paths = {
+        ("64bit", "ELF") : _opj(_mod_dir, "libtimsdata.so"),
+        ("32bit", "WindowsPE") : _opj(_mod_dir, "win32", "timsdata.dll"),
+        ("64bit", "WindowsPE") : _opj(_mod_dir, "win64", "timsdata.dll"),
+        # Probably CYGWIN:
+        ("32bit", "") : _opj(_mod_dir, "win32", "timsdata.dll"),
+        ("64bit", "") : _opj(_mod_dir, "win64", "timsdata.dll"),
+     }
 
 
 def get_so_paths():
-    return _paths
+    try:
+        return [_paths[platform.architecture()]] + list(set(_paths.values()) -  set([_paths[platform.architecture()]]))
+    except KeyError:
+        # unsupported platform: return everything and hope for the best
+        return list(set(_paths.values()))
